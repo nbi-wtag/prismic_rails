8bad123992be51d9a653be2442235ceda3ffcb68
Felix Langenegger <f.langenegger@fadendaten.ch>
2017-06-01 17:20:05 +0200

    Update PrismicRails Module

--- lib/prismic-rails.rb
+++ lib/prismic-rails.rb
@@ -3,4 +3,16 @@ require 'prismic-rails/railtie' if defined?(Rails)
 
 module PrismicRails
 
+  class Config
+    attr_accessor :url, :token, :client_id, :client_secret
+  end
+
+  def self.config
+    @@config || Config.new
+  end
+
+  def self.configure
+    yield self.config
+  end
+
 end
