befefabef5d5c03326fbe936cc5c1f614deab4e6
Felix Langenegger <f.langenegger@fadendaten.ch>
2017-06-01 16:38:50 +0200

    Version up

--- lib/prismic-rails/railtie.rb
+++ lib/prismic-rails/railtie.rb
@@ -4,20 +4,8 @@ module PrismicRails
   class Railtie < Rails::Railtie
     config.prismic = ActiveSupport::OrderedOptions.new # enable namespaced configuration in Rails environments
 
-    initializer ".configure" do |app|
-      PrismicRails.configure do |config|
-        config.key = app.config.prismic_rails[:key] || self.app_name
-      end
-    end
-
     initializer "welcome.helper" do
       ActionView::Base.send :include, ViewHelpers
-    end
-
-    private
-
-    def self.app_name
-      ::Rails.application.class.to_s.split("::").first
     end
   end
 end
--- lib/prismic-rails/version.rb
+++ lib/prismic-rails/version.rb
@@ -1,3 +1,3 @@
 module PrismicRails
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
