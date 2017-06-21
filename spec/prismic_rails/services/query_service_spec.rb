44a461a8fd67ebfbf6ac7f69363865f12bbb1363
fho-wtag <faruk.hossain@welltravel.com>
2017-06-21 16:04:00 +0600

    Add spec

--- spec/prismic_rails/services/query_service_spec.rb
+++ spec/prismic_rails/services/query_service_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe PrismicRails::QueryService do
         expect(subject.type('text')).to be_a(Prismic::Response)
       end
     end
+
+    context 'with language set' do
+      before do
+        allow_any_instance_of(PrismicRails::LanguageService).to receive(:language_hash).and_return({ en: 'en-us' })
+      end
+      it 'should return a prismic response' do
+        expect(subject.type('text', { lang: 'en' })).to be_a(Prismic::Response)
+      end
+    end
   end
 end
